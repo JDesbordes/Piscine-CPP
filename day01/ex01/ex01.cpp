ed9e29da50d3c10c9f8e2021a2b59fb4fa076411
Jean-jacques Acens <jacens@z2r3p3.le-101.fr>
2020-02-22 17:51:51 +0100

    ex01

---
+++ day01/ex01/ex01.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ex01.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jacens <jacens@student.le-101.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/22 17:47:02 by jacens            #+#    #+#             */
+/*   Updated: 2020/02/22 17:50:49 by jacens           ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+void memoryLeak()
+{
+	std::string* panthere = new std::string("String panthere");
+	std::cout << *panthere << std::endl;
+	delete panthere;
+}
+
+int	main(void)
+{
+	memoryLeak();
+	return (1);
+}
