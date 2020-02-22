93f44f8cc9616ff9109b3cf181f597711235c829
Jean-jacques Acens <jacens@z2r3p3.le-101.fr>
2020-02-22 11:42:03 +0100

    correct ortho

--- day00/ex01/srcs/my_awsome_phonebook.cpp
+++ day00/ex01/srcs/my_awsome_phonebook.cpp
@@ -6,7 +6,7 @@
 /*   By: jacens <jacens@student.le-101.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/20 15:46:35 by jacens            #+#    #+#             */
-/*   Updated: 2020/02/21 03:20:58 by jacens           ###   ########lyon.fr   */
+/*   Updated: 2020/02/22 11:39:51 by jacens           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -132,7 +132,7 @@ static void	search_contact(Contact *contacts, int i)
 		{
 			std::cin.clear();
       		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-			std::cout << "\n\033[0;31mYour select is not an int.\033[0m\n";
+			std::cout << "\n\033[0;31mYour selection is not an int.\033[0m\n";
 		}
 		else if (j - 1 > i)
 			std::cout << "\n\033[0;31mIndex " << j << " not exist.\033[0m\n\n";
