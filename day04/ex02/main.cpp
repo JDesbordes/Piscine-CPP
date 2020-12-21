52d67ed933397d0885151752a7a0a3f997b41562
AcensJJ <uentosacens@gmail.com>
2020-12-21 18:03:26 +0100

    ok main

--- day04/ex02/main.cpp
+++ day04/ex02/main.cpp
@@ -6,11 +6,21 @@ int main2()
 {
 	ISpaceMarine* bob = new TacticalMarine;
 	ISpaceMarine* jim = new AssaultTerminator;
+	TacticalMarine *b = new TacticalMarine();
+	AssaultTerminator *j = new AssaultTerminator();
+	ISpaceMarine* bib = new TacticalMarine(*b);
+	ISpaceMarine* beb = new AssaultTerminator(*j);
+	ISpaceMarine* jon;
+	ISpaceMarine* jun;
+	jon = jim;
+	jun = bob;
 	std::cout << std::endl;
 
 	ISquad* vlc = new Squad;
 	vlc->push(bob);
 	vlc->push(jim);
+	vlc->push(bib);
+	vlc->push(beb);
 	std::cout << std::endl;
 
 	for (int i = 0; i < vlc->getCount(); ++i)
@@ -24,6 +34,8 @@ int main2()
 	std::cout << std::endl;
 
 	delete vlc;
+	delete j;
+	delete b;
 	return 0;
 }
 
