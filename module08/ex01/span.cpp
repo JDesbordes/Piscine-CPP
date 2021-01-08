0e9e1c450df3d5004f949ac78f623ac548673a0b
AcensJJ <uentosacens@gmail.com>
2021-01-08 12:05:40 +0100

    span without const end

--- module08/ex01/span.cpp
+++ module08/ex01/span.cpp
@@ -4,21 +4,21 @@
 ** ------------------------------- CONSTRUCTOR --------------------------------
 */
 
-Span::Span() : array(new int[0]), size(0)
+Span::Span() : array(new int[0]), size(0), now(0)
 {
 }
 
-Span::Span( unsigned int n) : array(new int[n]), size(n)
+Span::Span( unsigned int n) : array(new int[n]), size(n), now(0)
 {
-
 }
 
 Span::Span( const Span & src ) : array(new int[src.getSize()]), size(src.getSize())
 {
-	for (unsigned int i = 0; i < size; i++)
-	{
+	unsigned int& n = const_cast<unsigned int&>(this->size);
+	n = src.getSize();
+	now = src.getNow();
+	for (unsigned int i = 0; i < src.getNow(); i++)
 		array[i] = src.array[i];
-	}
 }
 
 /*
@@ -37,8 +37,15 @@ Span::~Span()
 
 Span &				Span::operator=( Span const & rhs )
 {
-	Span arr(rhs);
-	return(arr);
+	if (this != &rhs)
+	{
+		unsigned int& n = const_cast<unsigned int&>(this->size);
+		n = rhs.getSize();
+		now = rhs.getNow();
+		for (unsigned int i = 0; i < rhs.getNow(); i++)
+			array[i] = rhs.array[i];
+	}
+	return *this;
 }
 
 /*
@@ -53,6 +60,34 @@ Span &				Span::operator=( Span const & rhs )
 unsigned int Span::getSize() const
 {
 	return (size);
+}
+
+unsigned int Span::getNow() const
+{
+	return (now);
+}
+
+void Span::addNumber(int nb)
+{
+	now < size ? array[now++] = nb : 0;
+}
+
+int Span::shortestSpan()
+{
+	int ishort(array[0]);
+
+	for (unsigned int i = 0; i < now; i++)
+		array[i] < ishort ? ishort = array[i]: 0;
+	return (ishort);
+}
+
+int Span::longestSpan()
+{
+	int ilongest(array[0]);
+
+	for (unsigned int i = 0; i < now; i++)
+		array[i] > ilongest ? ilongest = array[i]: 0;
+	return (ilongest);
 }
 
 /* ************************************************************************** */
