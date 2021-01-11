73951514161d4a14ed7d34bd35cf42c1e01bc154
AcensJJ <45235527+AcensJJ@users.noreply.github.com>
2021-01-11 18:43:25 +0100

    Update mutantstack.hpp

--- module08/ex02/mutantstack.hpp
+++ module08/ex02/mutantstack.hpp
@@ -24,14 +24,14 @@ class MutantStack : public std::stack<T>
 		typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
 		typedef typename std::stack<T>::container_type::const_reverse_iterator const_reverse_iterator;
 
-		iterator begin(){return std::stack<T>::c.begin();};
-		iterator end(){return std::stack<T>::c.end();};
-		const_iterator cbegin(){return std::stack<T>::c.cbegin();};
-		const_iterator cend(){return std::stack<T>::c.cend();};
-		reverse_iterator rbegin(){return std::stack<T>::c.rbegin();};
-		reverse_iterator rend(){return std::stack<T>::c.rend();};
-		const_reverse_iterator crbegin(){return std::stack<T>::c.crbegin();};
-		const_reverse_iterator crend(){return std::stack<T>::c.crend();};
+		iterator begin() {return std::stack<T>::c.begin();};
+		iterator end() {return std::stack<T>::c.end();};
+		const_iterator cbegin() const {return std::stack<T>::c.cbegin();};
+		const_iterator cend() const {return std::stack<T>::c.cend();};
+		reverse_iterator rbegin() {return std::stack<T>::c.rbegin();};
+		reverse_iterator rend() {return std::stack<T>::c.rend();};
+		const_reverse_iterator crbegin() const {return std::stack<T>::c.crbegin();};
+		const_reverse_iterator crend() const {return std::stack<T>::c.crend();};
 
 	private:
 
