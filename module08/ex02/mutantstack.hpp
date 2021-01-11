b22644baa2fffed5a82175ec1a594f14db6058fa
Jerome Desbordes <jdesbord@42LYON.FR>
2021-01-11 15:14:01 +0100

    mod08 ex01

--- module08/ex02/mutantstack.hpp
+++ module08/ex02/mutantstack.hpp
@@ -3,18 +3,19 @@
 
 # include <iostream>
 # include <string>
+# include <stack>
 
 template <class T>
-class MutantStack : public std::stack
+class MutantStack : public std::stack<T>
 {
 	public:
-		MutantStack() : std::stack<t>();
-		MutantStack<T>( MutantStack const & src ) : std::stack<t>(src);
-		virtual ~Mutantstack();
+		MutantStack() : std::stack<T>() {};
+		MutantStack<T>( MutantStack const & src ) : std::stack<T>(src) {};
+		virtual ~MutantStack();
 		MutantStack<T> &		operator=( MutantStack const & rhs )
 		{
 			if (*this != rhs)
-				*this = new stack<t>(rhs);
+				*this = new std::stack<T>(rhs);
 			return (*this);
 		};
 	
@@ -23,14 +24,14 @@ class MutantStack : public std::stack
 		typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
 		typedef typename std::stack<T>::container_type::const_reverse_iterator const_reverse_iterator;
 
-		std::stack<T>::container_type::iterator beg(){return this.begin();};
-		std::stack<T>::container_type::iterator end(){return this.end();};
-		std::stack<T>::container_type::const_iterator cbeg();
-		std::stack<T>::container_type::const_iterator cend();
-		std::stack<T>::container_type::reverse_iterator rbeg();
-		std::stack<T>::container_type::reverse_iterator rend();
-		std::stack<T>::container_type::const_reverse_iterator crbeg();
-		std::stack<T>::container_type::const_reverse_iterator crend();
+		typename std::stack<T>::container_type::iterator begin(){return this.begin();};
+		typename std::stack<T>::container_type::iterator end(){return this.end();};
+		/*typename std::stack<T>::container_type::const_iterator begin();
+		typename std::stack<T>::container_type::const_iterator end();
+		typename std::stack<T>::container_type::reverse_iterator rbegin();
+		typename std::stack<T>::container_type::reverse_iterator rend();
+		typename std::stack<T>::container_type::const_reverse_iterator rbegin();
+		typename std::stack<T>::container_type::const_reverse_iterator rend();*/
 
 	private:
 
